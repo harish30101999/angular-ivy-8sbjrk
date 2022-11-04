0936f2731d8b7d8d65ac82f2e10b02defd92fd46
harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-11-04 16:10:54 +0530

    3 files modified

--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -7,6 +7,7 @@ import { FlightService } from './flight.service';
   styleUrls: ['flight.component.css'],
 })
 export class FlightComponent implements OnInit {
+  myImage:string="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVtDW6C65Xb6_W-ot54cP-h1bz8vh3K7QuzQ&usqp=CAU"
   flights = [];
   model = new flight();
   constructor(private flightService: FlightService) {}
